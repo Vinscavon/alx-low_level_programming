90daa6044819f167a98d44aa44bfbfc2ecc29f5d
Vinscavon <kipropvince@gmail.com>
2022-07-02 03:41:33 -0700

    alphabets to lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print alpabets in lower case  follow by new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
