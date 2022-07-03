1924d669963f69570b8309eb46af3f5ae1201ff8
Vinscavon <kipropvince@gmail.com>
2022-07-03 10:18:26 -0700

    Different combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * prints all possible different combinations of two digits
+ * Numbers must be separated by ,
+ * followed by a space
+ * The two digits must be different
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 58; n++)
+	{
+		for (m = 48; m <= 58; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 57 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
