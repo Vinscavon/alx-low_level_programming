71dee93c26aac7cb7669131f7f8c78f2bcc42de2
Vinscavon <kipropvince@gmail.com>
2022-07-03 11:07:30 -0700

    3 digit combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints all possible different combinations of three digits
+ * The three digits must be different
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	int n, m, l;
+
+	for (n = 48; n <= 58; n++)
+	{
+		for (m = 49; n <= 58; m++)
+		{
+			for (l = 50; l = 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
