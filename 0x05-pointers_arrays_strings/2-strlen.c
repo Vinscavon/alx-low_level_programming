fbf7e7e5cd2afe77c22d05170cbab0918b51678f
Vinscavon <kipropvince@gmail.com>
2022-07-11 13:08:19 -0700

    the legth of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string s to be measured
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	char s;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
